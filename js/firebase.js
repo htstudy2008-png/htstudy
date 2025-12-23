9dba512148fad54b14b5cb1c93bd084f38520f02
H&T STUDY <htstudy2008@gmail.com>
2025-12-23 20:53:57 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,13 +2,16 @@ import { initializeApp } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC7JAgI_eAdSWoGYEXySQYRyqPS4M5DWTU",
   authDomain: "htstudy-3c8ae.firebaseapp.com",
   projectId: "htstudy-3c8ae",
-  appId: "1:505638767150:web:07e67c52d3bb8fb7e02dbf"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
